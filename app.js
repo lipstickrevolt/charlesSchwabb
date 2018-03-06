55118d044282d1df0c1d161a47ae4836608284d0
lipstickrevolt <lipstickrevolt@gmail.com>
2018-03-06 11:48:26 -0700

    Update and rename jsbin.rubateh.js to app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+function resetColors(){
+	$("#redBox").css("background-color","red");
+	$("#blueBox").css("background-color","blue");
+	$("#greenBox").css("background-color","green");
+  }
+  function makeItRed(){
+	$("#blueBox").css("background-color","red");
+	$("#redBox").css("background-color","red");
+	$("#greenBox").css("background-color","red");
+	
+  }
+  function makeItGreen(){
+	 $("#redBox").css("background-color","green");
+	$("#blueBox").css("background-color","green");
+	$("#greenBox").css("background-color","green");
+  }
+  function makeItBlue(){
+	 $("#redBox").css("background-color","blue");
+	$("#greenBox").css("background-color","blue");
+	$("#blueBox").css("background-color","blue");
+  }
